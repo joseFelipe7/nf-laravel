c21ba406f57716e8c955cc5b64e19a90d3508704
joseFelipe7 <josefelipedas.santos@gmail.com>
2023-12-17 21:38:25 -0300

    feat(migrations) adicionando migrations de usuarios e nota fiscal

---
+++ database/migrations/2023_12_17_234658_create_nfs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nfs', function (Blueprint $table) {
+            $table->id();         
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('nf_code', 9);
+            $table->float('value');
+            $table->date('date_issue');
+            $table->string('sender_cnpj', 14);
+            $table->string('sender_name', 100);
+            $table->string('delivery_cnpj', 14);
+            $table->string('delivery_name', 100);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            //numero - Identificador único do documento String Deve possuir exatos 9 dígitos.
+            //valor  - Valor da nota fiscal Numérico Precisa ser maior que 0.
+            //data_emissao - Dia da emissão do documento. Data Não pode ser uma data no futuro.
+            //cnpj_remetente - Identificador do remetente da nota. String Precisa ter validação se é um cnpj válido.
+            //nome_remetente - Nome do remetente da nota String No máximo 100 caracteres.
+            //cnpj_transportador - Identificador do transportador da nota. String Precisa ter validação se é um cnpj válido.
+            //nome_transportador - Nome do transportador da nota String No máximo 100 caracteres.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nfs');
+    }
+};
